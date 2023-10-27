e9dc94a21ab06841c36d2dadddf06da047a18107
Ferdy R Cardona <ferdycard0na11@gmail.com>
2023-10-26 18:41:58 -0600

    Capa Datos y Negocios funcionando para Proveedores

---
+++ CapaDatos/CD_Proveedor.cs
@@ -0,0 +1,175 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_Proveedor
+    {
+
+        public List<Proveedor> Listar()
+        {
+            List<Proveedor> lista = new List<Proveedor>();
+
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("select idProveedor, nombre, telefono, correo, direccion from PROVEEDOR");
+
+                    SqlCommand cmd = new SqlCommand(query.ToString(), oconexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Proveedor
+                            {
+                                idProveedor = Convert.ToInt32(dr["idProveedor"]),
+                                Nombre = dr["nombre"].ToString(),
+                                Telefono = dr["telefono"].ToString(),
+                                Correo = dr["correo"].ToString(),                         
+                                Direccion = dr["direccion"].ToString(),                               
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lista = new List<Proveedor>();
+                }
+                return lista;
+            }
+        }
+
+        public int Registrar(Proveedor obj, out string Mensaje)
+        {
+            int idProveedorgenerado = 0;
+            Mensaje = string.Empty;
+
+            try
+            {
+
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarProveedor", oconexion);
+                    cmd.Parameters.AddWithValue("Nombre", obj.Nombre);
+                    cmd.Parameters.AddWithValue("Telefono", obj.Telefono);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    cmd.Parameters.AddWithValue("Direccion", obj.Direccion);
+
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oconexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    idProveedorgenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                idProveedorgenerado = 0;
+                Mensaje = ex.Message;
+            }
+
+
+
+            return idProveedorgenerado;
+        }
+
+        public bool Editar(Proveedor obj, out string Mensaje)
+        {
+            bool respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EditarProveedor", oconexion);
+                    cmd.Parameters.AddWithValue("IdProveedor", obj.idProveedor);
+                    cmd.Parameters.AddWithValue("Nombre", obj.Nombre);
+                    cmd.Parameters.AddWithValue("Telefono", obj.Telefono);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    cmd.Parameters.AddWithValue("Direccion", obj.Direccion);
+
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oconexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                Mensaje = ex.Message;
+            }
+
+            return respuesta;
+        }
+
+
+        public bool Eliminar(Proveedor obj, out string Mensaje)
+        {
+            bool respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EliminarProveedor", oconexion);
+                    cmd.Parameters.AddWithValue("IdProveedor", obj.idProveedor);
+
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oconexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                Mensaje = ex.Message;
+            }
+
+            return respuesta;
+        }
+
+    }
+}
---
+++ CapaNegocio/CN_Proveedor.cs
@@ -0,0 +1,84 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class CN_Proveedor
+    {
+
+        private CD_Proveedor objcd_Proveedor = new CD_Proveedor();
+
+        public List<Proveedor> Listar()
+        {
+            return objcd_Proveedor.Listar();
+        }
+
+        public int Registrar(Proveedor obj, out string Mensaje)
+        {
+            Mensaje = string.Empty; // declara mensaj como Cadena vacia para no ser null
+
+            if (obj.Nombre == "")
+            {
+                Mensaje += "Es necesario el nombre del Proveedor\n";
+            }
+            if (obj.Telefono == "")
+            {
+                Mensaje += "Es necesario el Telefono del Proveedor\n";
+            }
+            if (obj.Direccion == "")
+            {
+                Mensaje += "Es necesarioa la Direcció del proveedor\n";
+            }
+
+            if (Mensaje != string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                return objcd_Proveedor.Registrar(obj, out Mensaje);
+            }
+
+        }
+
+        public bool Editar(Proveedor obj, out string Mensaje)
+        {
+            Mensaje = string.Empty; // declara mensaj como Cadena vacia para no ser null
+
+            if (obj.Nombre == "")
+            {
+                Mensaje += "Es necesario el nombre del Proveedor\n";
+            }
+            if (obj.Telefono == "")
+            {
+                Mensaje += "Es necesario el Teléfono del nombre de Proveedor\n";
+            }
+            if (obj.Direccion == "")
+            {
+                Mensaje += "Es necesaria la direccion del Proveedor\n";
+            }
+
+            if (Mensaje != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return objcd_Proveedor.Editar(obj, out Mensaje);
+            }
+
+
+        }
+
+        public bool Eliminar(Proveedor obj, out string Mensaje)
+        {
+            return objcd_Proveedor.Eliminar(obj, out Mensaje);
+        }
+
+    }
+}
